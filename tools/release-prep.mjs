def1b13426da0cd9dcd24c5b5c2be53dee17011f
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2026-07-31 06:59:41 +0900

    add change

--- tools/release-prep.mjs
+++ tools/release-prep.mjs
@@ -2,6 +2,7 @@
 import { existsSync, readFileSync } from "node:fs";
 import { spawnSync } from "node:child_process";
 
+// changed
 const RELEASE_FILES = ["package.json", "package-lock.json", "CHANGELOG.md"];
 const REPOSITORY_URL = "https://github.com/KazuCocoa/renma";
 
